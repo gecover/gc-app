53884759a1711bd14db477dc09c43862d2e178ec
Alex <alexanderv1414@gmail.com>
2023-11-11 00:03:50 -0700

    Some Auth, Stripe and Page changes. V1 80% DONE

---
+++ app/resume/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <section className="bg-black">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:flex-col sm:align-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+            Coming soon!
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,31 +1,68 @@
-import { getSession } from '@/app/supabase-server';
-
-import { redirect } from 'next/navigation';
-import Logo from '@/components/icons/Logo';
+//'use client';
 
 import React from 'react';
 import Link from 'next/link';
-import { Card, CardActionArea, Typography } from '@mui/material';
+import { Stack, Card, CardActionArea, Typography } from '@mui/material';
 
+import { Session, User } from '@supabase/supabase-js';
+//import { useRouter } from 'next/navigation';
 
-export default async function Tools() {
-  //const session = await getSession();
+interface Props {
+  session: Session | null;
+  user: User | null | undefined;
+}
 
+export default function Tools({
+  session,
+  user
+}: Props) {
+  
+  //const router = useRouter();
   return (
-    <div className="flex justify-center height-screen-helper">
-      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80 ">
-        <div className="flex justify-center pb-12 ">
-            <Link href="/generate-cv" passHref>
-                <Card>
-                    <CardActionArea>
-                        <Typography gutterBottom variant="h5" component="div" sx={{ padding: '20px', textAlign: 'center' }}>
-                            Generate Cover Letter
-                        </Typography>
-                    </CardActionArea>
-                </Card>
-            </Link>
+    <section className="bg-black">
+      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:flex-col sm:align-center">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+            Gecover Tools
+          </h1>
+          {/* <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+             Generate custom tailored cover letters. Resume refiner coming soon.
+          </p> */}
+          <ToolButtons />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
+
+function ToolButtons() {
+  return (
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={3}
+      sx={{ margin: '48px' }}
+    >
+      <Link href="/generate-cv" passHref>
+        <Card>
+            <CardActionArea>
+                <Typography gutterBottom variant="h5" component="div" sx={{ padding: '20px', textAlign: 'center' }}>
+                    Generate Cover Letter
+                </Typography>
+            </CardActionArea>
+        </Card>
+      </Link>
+      <Link href="/resume" passHref>
+          <Card>
+              <CardActionArea>
+                  <Typography gutterBottom variant="h5" component="div" sx={{ padding: '20px', textAlign: 'center' }}>
+                      Refine Resume
+                  </Typography>
+              </CardActionArea>
+          </Card>
+      </Link>          
+    </Stack>
+  );
+}
+
