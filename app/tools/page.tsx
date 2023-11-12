de36e7e116df0779a95e66968efa06167f9a52b8
Alex <alexanderv1414@gmail.com>
2023-11-12 00:43:11 -0700

    V1 of Pages done
    - Auth Done
    - Endpoint integration done
    - V1 of PDF Download Done
    Small fixes todo
    Cyber security stuff in question

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,67 +1,32 @@
-//'use client';
-
 import React from 'react';
-import Link from 'next/link';
-import { Stack, Card, CardActionArea, Typography } from '@mui/material';
+import {
+  getSession,
+  getUserDetails,
+  getSubscription
+} from '@/app/supabase-server';
+import { redirect } from 'next/navigation';
+import ToolBar from './ToolBar';
 
-import { Session, User } from '@supabase/supabase-js';
-//import { useRouter } from 'next/navigation';
 
-interface Props {
-  session: Session | null;
-  user: User | null | undefined;
-}
+export default async function Tools() {
+  const [session, userDetails, subscription] = await Promise.all([
+    getSession(),
+    getUserDetails(),
+    getSubscription()
+  ]);
 
-export default function Tools({
-  session,
-  user
-}: Props) {
+  if (!session) {
+    return redirect('/signin');
+  }
   
-  //const router = useRouter();
-  return (
-    <section className="bg-black">
-      <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
-        <div className="sm:flex sm:flex-col sm:align-center">
-          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Gecover Tools
-          </h1>
-          {/* <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-             Generate custom tailored cover letters. Resume refiner coming soon.
-          </p> */}
-          <ToolButtons />
-        </div>
-      </div>
-    </section>
-  );
-}
+  // VALIDATE USER SUBSCRIPTION STATUS  
+  if (!subscription) {
+    return redirect('/');
+  }
 
-function ToolButtons() {
   return (
-    <Stack
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      spacing={3}
-      sx={{ margin: '48px' }}
-    >
-      <Link href="/generate-cv" passHref>
-        <Card>
-            <CardActionArea>
-                <Typography gutterBottom variant="h5" component="div" sx={{ padding: '20px', textAlign: 'center' }}>
-                    Generate Cover Letter
-                </Typography>
-            </CardActionArea>
-        </Card>
-      </Link>
-      <Link href="/resume" passHref>
-          <Card>
-              <CardActionArea>
-                  <Typography gutterBottom variant="h5" component="div" sx={{ padding: '20px', textAlign: 'center' }}>
-                      Refine Resume
-                  </Typography>
-              </CardActionArea>
-          </Card>
-      </Link>          
-    </Stack>
+    <div>
+      <ToolBar session={session}/>
+    </div>
   );
 }
---
+++ components/ui/PDF/PDFDocument.tsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import { Page, Text, View, Document, StyleSheet, Font } from '@react-pdf/renderer';
+
+const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'column',
+      backgroundColor: '#fff',
+      fontFamily: 'Helvetica', // Change to 'OpenSans' or your preferred font if registered above
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+    header: {
+      fontSize: 24,
+      textAlign: 'center',
+      color: 'black',
+      marginBottom: 20,
+    },
+    content: {
+      fontSize: 12,
+      lineHeight: 1.5,
+      textAlign: 'justify',
+      marginBottom: 20,
+    },
+    footer: {
+      fontSize: 12,
+      textAlign: 'left',
+      marginTop: 25,
+      paddingTop: 10,
+      borderTop: 1,
+      borderColor: '#eeeeee',
+    },
+    signature: {
+      marginTop: 15,
+      fontSize: 14,
+      textAlign: 'left',
+      fontFamily: 'Times-Roman',
+    },
+  });
+
+/*
+'Dear [Company Name] Team',
+'Good [time of day],',
+'Dear [Department] Team',
+'Hello [Company Name]',
+*/
+const introSet: string[] = [
+    'Hello',
+    'Hi',
+    'Greetings',
+    'Dear Hiring Manager',
+    'Good day',
+    'Hello and thank you for this opportunity',
+    'Hi there',
+    'Greetings of the day',
+    'Salutations',
+    'Warm greetings',
+    'To whom it may concern',
+    'I hope this message finds you well',
+    'Greetings to the [Department Name] team',
+    'It’s a pleasure to connect with you',
+    'I am excited to address this cover letter to you'
+];
+
+const closingSet: string[] = [
+    'Sincerely',
+    'Best regards',
+    'Kind regards',
+    'Yours truly',
+    'Warm regards',
+    'With best regards',
+    'Respectfully',
+    'Yours faithfully',
+    'Thank you for your consideration',
+    'With appreciation',
+    'Cordially',
+    'Looking forward to your response',
+    'With sincere thanks',
+    'Anticipating your reply',
+    'In gratitude',
+    'With eagerness for the opportunity',
+    'Best wishes',
+    'Thank you for your time and consideration',
+    'Hope to hear from you soon',
+    'Eagerly awaiting your reply'
+];
+
+interface PDFDocumentProps {
+    educationParagraph: string;
+    experienceParagraph: string;
+    position: string; 
+    companyName: string; 
+  }
+  
+  const PDFDocument: React.FC<PDFDocumentProps> = ({
+    educationParagraph,
+    experienceParagraph,
+    position="POSITION A", // ADD POSITION, COMPANY, AND INDIVIDUAL NAME ENDPOINTS
+    companyName="COMPANY A",
+  }) => {
+    const randomIntro: string = introSet[Math.floor(Math.random() * introSet.length)].replace('[Company Name]', companyName);
+    const randomClosing: string = closingSet[Math.floor(Math.random() * closingSet.length)];
+  
+    return (
+      <Document>
+        <Page size="A4" style={styles.page}>
+          <View style={styles.section}>
+            <Text style={styles.content}>{randomIntro}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.content}>I am applying for the {position} position at {companyName}.</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.content}>{educationParagraph}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.content}>{experienceParagraph}</Text>
+          </View>
+          <View style={styles.section}>
+            <Text style={styles.content}>I am motivated by the opportunity to join your team and contribute to your company's success.</Text>
+          </View>
+          <View style={styles.signature}>
+            <Text>{randomClosing}</Text>
+          </View>
+          <View style={styles.footer}>
+            <Text>Your Name</Text>
+            <Text>Your Contact Information</Text>
+          </View>
+        </Page>
+      </Document>
+    );
+  };
+  
+  export default PDFDocument;
