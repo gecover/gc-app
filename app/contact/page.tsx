6ee8be4c05424389b3ef5445bd9c5ad0465d5bd4
Alex <alexanderv1414@gmail.com>
2023-11-16 00:02:13 -0700

    issues

---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+    getSession
+  } from '@/app/supabase-server';
+import Contact from './Contact';
+
+
+export default async function ContactPage() {
+    // const [session] = await Promise.all([
+    //     getSession()
+    //   ]);
+    
+      return (
+        <Contact
+          //session={session}
+        />
+      );
+}
