87b68a8c2110a234bbad9d8522c652e553cfee49
Alex <alexanderv1414@gmail.com>
2023-11-16 13:28:45 -0700

    tutorial + static images

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import Navbar from '@/components/ui/Navbar';
 import { PropsWithChildren } from 'react';
 import 'styles/main.css';
 
+import Link from 'next/link';
+
 const meta = {
   title: 'gecover - Automated, Tailored Cover Letters',
   description: 'Brought to you by GeCover LLC.',
@@ -57,9 +59,15 @@ export default function RootLayout({
             {children}
           </main>    
           <footer className="flex flex-row justify-center bg-gray-800 text-white p-4 text-center">
-              <p className="m-2">&copy; {new Date().getFullYear()} GeCover. All rights reserved.</p>
+              <Link
+                href="/contact"
+                className="m-2"
+              >
+                Contacts
+              </Link>
+              <p className="font-bold m-2">&copy; {new Date().getFullYear()} GeCover. All rights reserved.</p>
               <a className="m-2" href="https://www.termsfeed.com/live/15cd706e-a80f-4ad0-a1cc-34f68716ad27"> Privacy Policy</a>
-          </footer>    
+          </footer>
         </SupabaseProvider>
       </body>
     </html>
--- app/tutorial/page.tsx
+++ app/tutorial/page.tsx
@@ -1,4 +1,5 @@
 import Box from '@mui/material/Box';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -6,13 +7,36 @@ export default function Page() {
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-12 lg:px-8">
         <div className="flex flex-col sm:flex sm:flex-col align-center justify-center">
           <h1 className="text-4xl font-extrabold text-white  sm:text-4xl">
-            How to
+            How to Generate Your First Gecover
           </h1>
           <Box className="">
-             <p className='pt-4 text-lg'> 1: Sign in to our platform </p>
-             <p className='pt-4 text-lg'> 2: Navigate to the demo </p>
-             <h2 className="text-2xl pt-8 font-extrabold text-white sm:text-2xl"> On desktop </h2>
-             <p className='pt-4 text-lg'> 3: Upload your pdf </p>
+             <p className='pt-4 text-lg'>1 - Sign in to your Gecover account. 
+              <br></br>
+              <sub>
+                <span className="underline">Note:</span> If you do not have an account, you can register using your email or one of our providers.
+              </sub>
+            </p>
+            <Image 
+              src="/tutorial-signin.png" 
+              alt="Description of the image" 
+              width={800}
+              height={600}
+            />
+            <p className='pt-4 text-lg'>2 - Navigate to Tutorial</p>
+            <Image 
+              src="/tutorial-tutorial.png" 
+              alt="Description of the image" 
+              width={800}
+              height={600}
+            />
+            <h2 className="text-2xl pt-8 font-extrabold text-white sm:text-2xl"> Web Browser: </h2>
+            <p className='pt-4 text-lg'> 3: Upload your Resume. 
+            <br></br>
+            <sub>
+              <span className="underline">Note:</span> Currently we only support PDF format.
+            </sub>
+            </p>
+
              <p className='pt-4 text-lg'> 4: Upload the LinkedIn link to the job posting(currently working on making this easier) </p>
              <p className='pt-4 text-lg'> 4.1: Go to LinkedIn jobs </p>
              <p className='pt-4 text-lg'> 4.2: Click on the job you want to apply to </p>
