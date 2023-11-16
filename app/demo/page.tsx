8d5fa29d7a00a795f74caa86bdc2c8fc33c29a94
vaughanlove <vaughanlove1@gmail.com>
2023-11-16 01:13:24 -0500

    ux fixes

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -13,17 +13,25 @@ export default async function GenerateCoverLetter() {
     getUserDetails()
   ]);
 
-  if (!session) {
-    return redirect('/signin');
-  }
+  // if (!session) {
+  //   return redirect('/signin');
+  // }
 
-  if (!userDetails) {
-    return redirect('/signin');
-  }
-  let name = ''
-  if (userDetails.full_name){
-    name = userDetails.full_name
-  }
+  // if (!userDetails) {
+  //   return redirect('/signin');
+  // }
+  
+
+  let name = 'you need to enter your name in account'
+  // try{
+  //   if (userDetails.full_name){
+  //     name = userDetails.full_name
+  //   }
+  // } catch {
+  //   // todo: add a redirect to /account paired with an error message to user.
+  //   console.error("user needs to input their name on the account.")
+  // }
+  
 
   return (
     <div>
