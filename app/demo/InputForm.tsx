b4c5797f2ba680c662c2cf28571a217577334131
vaughanlove <vaughanlove1@gmail.com>
2023-11-29 10:18:15 -0700

    auth headers

--- app/demo/InputForm.tsx
+++ app/demo/InputForm.tsx
@@ -136,7 +136,7 @@ export default function InputForm({ session, userName }: Props) {
       const urlList = await axios.post(`${process.env.API_URL}/extract_url/`, 
       { url },
       {headers: {
-        // 'Authorization' : `Bearer ${session.access_token}`
+        'Authorization' : `Bearer ${session.access_token}`
       }});
       if (!urlList.data.contents) {
         setOpen(true);
@@ -187,7 +187,7 @@ export default function InputForm({ session, userName }: Props) {
             // cancelToken: source.token,
             timeout: TIMEOUT_DURATION,
             headers: {
-              // 'Authorization' : `Bearer ${session.access_token}`
+              'Authorization' : `Bearer ${session.access_token}`
             },
             params: { requirements }
         });
